ecf0ded0000e09389268ba1bcb3a72f52bda9bb7
Eugene Everson <ee@pinkaxe.com>
2009-10-10 16:57:11 +0900

    sys: Add thread.h

---
+++ lib/sys/thread.h
@@ -0,0 +1,109 @@
+#ifndef __THREAD_H__
+#define __THREAD_H__
+
+#include<pthread.h>
+
+
+
+#ifdef NDEBUG
+
+#define dbg_thread(call) call
+
+#else
+
+#define dbg_thread(call) \
+{ \
+	int ret; \
+	if((ret=(call)) != 0){ \
+        printf("cunt\n"); \
+		log4(LERR, "%s:%d:%s Error: %s\n", __FILE__, __LINE__, \
+			#call, strerror(ret)); \
+	} \
+}
+
+#endif
+
+typedef pthread_t thread_t;
+typedef pthread_mutex_t mutex_t;
+typedef	pthread_cond_t cond_t;
+
+#define mutex_init(mutex, data) \
+	dbg_thread(pthread_mutex_init(mutex, data))
+
+#define cond_init(cond, data) \
+	dbg_thread(pthread_cond_init(cond, data))
+
+#define thread_create(tid, data, func, arg) \
+	dbg_thread(pthread_create(tid, data, func, arg))
+
+#define thread_cancel(tid) \
+	pthread_cancel(tid)
+
+#define mutex_destroy(mutex) \
+	pthread_mutex_destroy(mutex)
+
+#define	cond_destroy(cond) \
+	dbg_thread(pthread_cond_destroy(cond))
+
+#define thread_self() \
+    pthread_self()
+
+#define thread_detach(tid) \
+	dbg_thread(pthread_detach(tid))
+
+#define mutex_lock(mutex) \
+	dbg_thread(pthread_mutex_lock(mutex))
+
+#define mutex_unlock(mutex) \
+	dbg_thread(pthread_mutex_unlock(mutex))
+
+#define cond_wait(cond, mutex) \
+	dbg_thread(pthread_cond_wait(cond, mutex))
+
+#define cond_timedwait(tid, data, timespec) \
+	pthread_cond_timedwait(tid, data, timespec)
+
+#define cond_signal(cond) \
+	dbg_thread(pthread_cond_signal(cond))	
+
+#define thread_testcancel() \
+	pthread_testcancel() /* returns void */
+
+
+// if  NO THREADS
+//
+#if 0
+typedef void *thread_t;
+typedef void *mutex_t;
+typedef	void *cond_t;
+
+#define mutex_init(mutex, data)
+
+#define cond_init(cond, data)
+
+#define thread_create(tid, data, func, arg)
+
+#define thread_cancel(tid)
+
+#define mutex_destroy(mutex)
+
+#define	cond_destroy(cond)
+
+#define thread_self()
+
+#define thread_detach(tid)
+
+#define mutex_lock(mutex)
+
+#define mutex_unlock(mutex)
+
+#define cond_wait(cond, mutex)
+
+#define cond_timedwait(tid, data, timespec)
+
+#define cond_signal(cond)
+
+#define thread_testcancel()
+#endif
+
+#endif
